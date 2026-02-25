2dc7d725d3b92506bbf57deafebaa2c042e15924
Rina Plata <rinamarcelaplata@gmail.com>
2026-02-25 12:48:17 -0500

    feat: update routing and product service to use slugs instead of IDs

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -9,17 +9,24 @@ import { environment } from "@env/environment";
 export class ProductService {
   private http = inject(HttpClient);
 
-  getProducts(category_id?: string) {
+  getProducts(param: { category_id?: string; category_slug?: string }) {
     const url = new URL(`${environment.apiUrl}/api/v1/products`);
-    if (category_id) {
-      url.searchParams.set("categoryId", category_id);
+    if (param.category_id) {
+      url.searchParams.set("categoryId", param.category_id);
+    }
+    if (param.category_slug) {
+      url.searchParams.set("categorySlug", param.category_slug);
     }
     return this.http.get<Product[]>(url.toString());
   }
 
-  getOne(id: string) {
-    return this.http.get<Product>(
-      `${environment.apiUrl}/api/v1/products/${id}`,
-    );
+  getOne(param: { product_id?: string; product_slug?: string }) {
+    if (!param.product_id && !param.product_slug) {
+      throw new Error("product_id or product_slug is required");
+    }
+    const endpoint = param.product_id
+      ? `${environment.apiUrl}/api/v1/products/${param.product_id}`
+      : `${environment.apiUrl}/api/v1/products/slug/${param.product_slug}`;
+    return this.http.get<Product>(endpoint);
   }
 }
